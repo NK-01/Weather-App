dfbf3e4246443a943484dd301d5bccd8f590faa6
Nidhi Kumari <72823658+NK-01@users.noreply.github.com>
2023-02-19 21:04:25 +0530

    Added API integration for current temperature and other weather information

---
+++ srcs/app.js
@@ -0,0 +1,57 @@
+function formatCityName(city) {
+  city = city.trim();
+  let formattedCityName = "";
+  let cityArr = city.split(` `);
+  for (let i = 0; i < cityArr.length; ++i) {
+    if (cityArr[i].length !== 0) {
+      cityString = cityArr[i];
+      formattedCityName +=
+        cityString[0].toUpperCase() + cityString.slice(1).toLowerCase();
+      formattedCityName += " ";
+    }
+  }
+  formattedCityName = formattedCityName.slice(0, -1);
+  return formattedCityName;
+}
+
+function displayWeatherInformations(response) {
+  let currentTemperatureElement = document.querySelector(
+    "#current-temperature"
+  );
+  let realFeelElement = document.querySelector("#real-feel");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let pressureElement = document.querySelector("#pressure");
+
+  let currentTemperature = Math.round(response.data.main.temp);
+  let realFeel = Math.round(response.data.main.feels_like);
+  let humidity = Math.round(response.data.main.humidity);
+  let pressure = Math.round(response.data.main.pressure);
+  let windSpeed = Math.round(response.data.wind.speed);
+  currentTemperatureElement.innerHTML = currentTemperature;
+  realFeelElement.innerHTML = realFeel;
+  humidityElement.innerHTML = humidity;
+  windSpeedElement.innerHTML = windSpeed;
+  pressureElement.innerHTML = pressure;
+}
+
+function search(event) {
+  event.preventDefault();
+  let temperatureUnits = `metric`;
+  let city = document.querySelector(`#search-city`);
+  city.value = formatCityName(city.value);
+
+  if (city.value) {
+    let displayCityName = document.querySelector(`#city-name`);
+    displayCityName.innerHTML = city.value;
+
+    let apiKey = `b20ecfd3dab44a1228a1285e56521d52`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=${temperatureUnits}`;
+    axios.get(apiUrl).then(displayWeatherInformations);
+  } else {
+    alert(`Please enter a city name`);
+  }
+}
+
+let searchForm = document.querySelector(`.search-bar`);
+searchForm.addEventListener(`submit`, search);
